bb886f3d9a0fd1c22386c050d57b1a130e38c741
Clever-programm <edik22217@gmail.com>
2023-02-06 18:55:14 +0400

    new func

--- main.py
+++ main.py
@@ -3,11 +3,29 @@ import time
 
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
 from config import BOT_TOKEN
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
 )
 logger = logging.getLogger(__name__)
+reply_keyboard = [['/time', '/phone'],
+                  ['/close', '/work_time']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
+
+async def start(update, context):
+    await update.message.reply_text(
+        "Я бот-CleverIT. Какая информация вам нужна?",
+        reply_markup=markup
+    )
+
+
+async def close_keyboard(update, context):
+    await update.message.reply_text(
+        "Ok",
+        reply_markup=ReplyKeyboardRemove()
+    )
 
 
 async def echo(update, context):
@@ -15,15 +33,19 @@ async def echo(update, context):
 
 
 async def ftime(update, context):
-    user = update.effective_user
     await update.message.reply_text(time.asctime().split()[3])
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     text_handler = MessageHandler(filters.TEXT, echo)
+    start_handler = CommandHandler('start', start)
     time_handler = CommandHandler('time', ftime)
+    close_handler = CommandHandler('close', close_keyboard)
+    application.add_handler(start_handler)
     application.add_handler(time_handler)
+    application.add_handler(close_handler)
+    application.add_handler(text_handler)
     application.run_polling()
 
 
